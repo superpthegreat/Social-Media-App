f9955e8a853123a03c04c9f0320540b46fbbf343
superpthegreat <anthony.soriano453@gmail.com>
2023-04-16 18:43:55 -0700

    reinstalled npm packages

--- my-app/src/scenes/navbar/index.jsx
+++ my-app/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          My Social Media App
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
